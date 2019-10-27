94401ce78bc6714d8b32eec834e9065a153db1e9
Yi Chen <yc311@duke.edu>
2019-10-27 19:26:17 -0400

    implements

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,6 +1,40 @@
-public class LinkStrand {
+public class LinkStrand implements IDnaStrand {
     //Sanika Gupte
     //Yi Chen
     public String hello = new String("Hello world");
 
+    @Override
+    public long size() {
+        return 0;
+    }
+
+    @Override
+    public void initialize(String source) {
+
+    }
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand append(String dna) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        return null;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
 }
