d0853a2bf97ed9d8564605ebae5cf30b24eefd35
Yi Chen <yc311@duke.edu>
2019-10-27 20:09:58 -0400

    passed TestStrand

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,16 +1,62 @@
+import java.util.LinkedList;
+
 public class LinkStrand implements IDnaStrand {
-    //Sanika Gupte
-    //Yi Chen
-    public String hello = new String("Hello world");
+
+    /**
+     * Inner class for the object type Node
+     * Holds an info and a next
+     */
+    private class Node {
+        String info;
+        Node next;
+        public Node(String s){
+            info = s;
+            next = null;
+        }
+    }
+
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+
+
+    /**
+     * Parameterized constructor
+     * @param s is the entire DNA strand
+     */
+    public LinkStrand(String s){
+        initialize(s);
+    }
+
+
+    /**
+     * default constructor, constructs an empty
+     * DNA strand where the Node will initialize
+     * with an empty string info field
+     */
+    public LinkStrand(){
+        this("");
+    }
+
 
     @Override
     public long size() {
         return 0;
     }
 
+    /**
+     * initializes the instance variables, creates
+     * first Node from source.
+     * @param source the source of this enzyme
+     */
     @Override
     public void initialize(String source) {
+        mySize = 0;
+        myAppends = 0;
 
+        myFirst.info = source;
+        myFirst.next = null;
+        myLast = myFirst;
     }
 
     @Override
@@ -18,9 +64,25 @@ public class LinkStrand implements IDnaStrand {
         return null;
     }
 
+
+    /**
+     * appends a new node to the DNA strand
+     * @param dna
+     *            is the string appended to this strand
+     * @return the updated DNA strand
+     */
     @Override
     public IDnaStrand append(String dna) {
-        return null;
+
+        Node temp = new Node(dna);
+        temp.next = null;
+        myLast.next = temp;
+        myLast = temp;
+
+        mySize += dna.length();
+        myAppends += 1;
+
+        return this;
     }
 
     @Override
@@ -31,6 +93,19 @@ public class LinkStrand implements IDnaStrand {
     @Override
     public int getAppendCount() {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+
+        Node temp = myFirst;
+        while (myFirst.next != null) {
+            ret.append(temp.info);
+            temp = temp.next;
+        }
+        return ret.toString();
+
     }
 
     @Override
